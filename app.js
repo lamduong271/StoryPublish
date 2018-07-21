a3d9390fe450c54d271ca093d92227193dbca524
Lam <lam.duong@polku.io>
2018-07-22 00:32:16 +0300

    handle auth

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+//passport config
+require('./config/passport')(passport);
+//loasd routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('It work');
 })
+
+//use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI: 'mongodb: //<lam>:<lam123@ds247101.mlab.com:47101/store-book',
+    googleClientID: '102436108141-5s8bis08bqnu25ei63q7jtite9d2mrpb.apps.googleusercontent.com',
+    googleClientSecret: 'VDlx_m872pg67ekOO7cwoxYq'
+}
